6c4bdf8a1886b7b7d015f7775716026c0823de13
2Istvann <ifjsuranyiistvan@gmail.com>
2025-03-07 08:33:01 +0100

    Const

--- foglalas/src/stores/fog.js
+++ foglalas/src/stores/fog.js
@@ -1,16 +1,31 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
-import axios from 'axios'
+import axios, { Axios } from 'axios'
 import { useToast } from 'vue-toastification'
 
 export const useIdoStore = defineStore('ido', () => {
   const ido = ref([])
-
+  const availableTime = []
+  const bookedTime = []
   const loadAll = () => {
-    fetch("http://localhost:3000/idopontok")
+  ('http://localhost:3000/idopontok')
       .then(resp => resp.json())
       .then(data => ido.value = data)
   }
+  const Time = () => {
+    const idok = []
+    for(i = 8; i <= 16; i++){
+      idok.push(`${i}:00`)
+    }
+    this.availableTime = idok.filter(t => !this.bookedTime.includes(t)) 
+  }
 
-  return { loadAll  }
+  const Foglalas = () => {
+    axios.post('https://localhost:3000/idopontok', {time, name, phone})
+    this.bookedTime.push(time)
+    this.Time()
+  }
+
+  return { loadAll, Time, Foglalas }
+
 })
